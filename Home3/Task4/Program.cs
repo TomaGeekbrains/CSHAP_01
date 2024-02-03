2e839b6cf002b7f237f0e699afc48e87a98f57b7
Tamara Terekhova <terekhova.toma@inbox.ru>
2024-02-03 16:38:37 +0700

    Third task

---
+++ Home3/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход
+//  целое число из отрезка [10, 99] и показывает наибольшую цифру 
+// числа.
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write(" [10, 99]: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int firstDigit = number / 10;
+        int secondDigit = number % 10;
+        int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+         Console.WriteLine(maxDigit);
+    }
+}
