11649b1c70795927620fdccc01e31db1a9faa95a
Tamara Terekhova <terekhova.toma@inbox.ru>
2024-02-03 18:49:05 +0700

    lesson1:task2

--- lesson_01/Task_2/Program.cs
+++ lesson_01/Task_2/Program.cs
@@ -16,3 +16,10 @@ double product3 = i1 * d1;
 System.Console.WriteLine(product1);
 System.Console.WriteLine(product2);
 System.Console.WriteLine(product3);
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
