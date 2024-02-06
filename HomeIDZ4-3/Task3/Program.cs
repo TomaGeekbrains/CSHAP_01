24c0003a933e79f86028e91dd4ccbe65320fd8ea
Tamara Terekhova <terekhova.toma@inbox.ru>
2024-02-07 00:45:43 +0700

    Task3

---
+++ HomeIDZ4-3/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// // (первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+Console.Clear();
+int[] array = GetArray(10, 0, 20);
+Console.WriteLine(string.Join(" ", array));
+
+int[] reverseArray = ReversArray1(array);
+Console.WriteLine(string.Join(" ", reverseArray));
+
+// ReversArray2(array);                               // второй вариант решения
+// Console.WriteLine(string.Join(" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] ReversArray1(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for(int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
