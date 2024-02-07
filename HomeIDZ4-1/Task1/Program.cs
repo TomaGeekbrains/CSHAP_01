65e33be3b3f79ec4b951aa0f3028327ed24873f2
Tamara Terekhova <terekhova.toma@inbox.ru>
2024-02-07 12:33:00 +0700

    Task1

---
+++ HomeIDZ4-1/Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое число:");
+            string input = Console.ReadLine();
+
+            if (input == "q")
+            {
+                break; // выход из цикла при вводе символа 'q'
+            }
+
+            int number;
+            if (int.TryParse(input, out number))
+            {
+                int sumOfDigits = CalculateSumOfDigits(number);
+                if (sumOfDigits % 2 == 0)
+                {
+                    break; // выход из цикла при вводе числа, сумма цифр которого чётная
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q' для выхода.");
+            }
+        }
+    }
+
+    static int CalculateSumOfDigits(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
