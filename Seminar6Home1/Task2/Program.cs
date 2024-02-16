e802ffc1d94ff1823896b29c00e5cf71f650946e
Tamara Terekhova <terekhova.toma@inbox.ru>
2024-02-16 21:24:52 +0700

    Task2

--- Seminar6Home1/Task2/Program.cs
+++ Seminar6Home1/Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string originalString = "HeLloWorLD";
+        string lowerCaseString = originalString.ToLower();
+
+        Console.WriteLine(lowerCaseString);
+    }
+}
